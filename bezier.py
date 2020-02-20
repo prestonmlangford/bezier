3f2c0517a27a28e878c0f016acd6d03c4cfab049
prestonmlangford <prestonmlangford@gmail.com>
2020-02-19 20:44:28 -0600

    refined approach

--- bezier.py
+++ bezier.py
@@ -48,210 +48,222 @@ def algebras():
     print("Bezier Polynomial")
     print(sack(expr))
 
+    x = Symbol('x')
+    
+
+    # Factorial function
+    def factorial(n):
+        if n <= 0:
+            return 1
+        else:
+            return n*factorial(n-1)
+
+    # Taylor approximation at x0 of the function 'function'
+    def taylor(function,x0,n):
+        i = 0
+        p = 0
+        while i <= n:
+            p = p + (function.diff(x,i).subs(x,x0))/(factorial(i))*(x-x0)**i
+            i += 1
+        return p
+    for i in [0,1,2,3]:
+        ts = collect(expand(taylor(x**0.5,0.5,i)),x)
+        print("Talyor Expansion Order {}".format(i))
+        print(sack(ts))
+
 #algebras()
 
+ops = 0
 
-#https://en.wikipedia.org/wiki/Golden-section_search    
-"""Python program for golden section search.  This implementation
-   reuses function evaluations, saving 1/2 of the evaluations per
-   iteration, and returns a bounding interval."""
-
-invphi = (math.sqrt(5) - 1) / 2  # 1 / phi
-invphi2 = (3 - math.sqrt(5)) / 2  # 1 / phi^2
-
-def gss(f, a, b, tol=1e-5):
-    """Golden section search.
-
-    Given a function f with a single local minimum in
-    the interval [a,b], gss returns a subset interval
-    [c,d] that contains the minimum with d-c <= tol.
-
-    Example:
-    >>> f = lambda x: (x-2)**2
-    >>> a = 1
-    >>> b = 5
-    >>> tol = 1e-5
-    >>> (c,d) = gss(f, a, b, tol)
-    >>> print(c, d)
-    1.9999959837979107 2.0000050911830893
-    """
-
-    (a, b) = (min(a, b), max(a, b))
-    h = b - a
-    if h <= tol:
-        return (a, b)
-
-    # Required steps to achieve tolerance
-    n = int(math.ceil(math.log(tol / h) / math.log(invphi)))
-    print(n)
-    c = a + invphi2 * h
-    d = a + invphi * h
-    yc = f(c)
-    yd = f(d)
-
-    for k in range(n-1):
-        if yc < yd:
-            b = d
-            d = c
-            yd = yc
-            h = invphi * h
-            c = a + invphi2 * h
-            yc = f(c)
-        else:
-            a = c
-            c = d
-            yc = yd
-            h = invphi * h
-            d = a + invphi * h
-            yd = f(d)
-
-    if yc < yd:
-        t = (a+d)/2
-        #return (a, d)
-    else:
-        t = (c+b)/2
-        #return (c, b)
-    return t
 
 def add(X,Y):
+    global ops
+    ops +=1
     return (X[0]+Y[0],X[1]+Y[1])
 
 def sub(X,Y):
+    global ops
+    ops +=1
     return (X[0]-Y[0],X[1]-Y[1])
 
 def dot(X,Y):
+    global ops
+    ops +=2
     return X[0]*Y[0]+X[1]*Y[1]
 
-#https://www.wyzant.com/resources/answers/607711/determine-which-side-of-a-line-a-point-lies
-def det(P,X,Y):
-    #d=(x-x1)(y2-y1) - (y-y1)(x2-x1)
-    return (P[0]-X[0])*(Y[1]-X[1])-(P[1]-X[1])*(Y[0]-X[0])
+def det(A,B): 
+    global ops
+    ops +=3
+    return A[0]*B[1]-B[0]*A[1]
+    
+def norm2(X):
+    return dot(X,X)
     
 def mul(k,X):
+    global ops
+    ops +=1
     return (X[0]*k,X[1]*k)
 
-def dist(t,Alpha,Beta,Gamma):
-    D = add(Gamma,mul(t,add(Beta,mul(t,Alpha))))
-    return dot(D,D)
+def clamp(x,lower,upper):
+    global ops
+    ops += 2
+    return min(max(lower,x),upper)
 
-def bez(t,A,B,C):
-    return add(add(mul((1-t)*(1-t),A),mul(2*(1-t)*t,B)),mul(t*t,C))
+def fma(X,y,Z):
+    global ops
+    ops +=1
+    return add(mul(y,X),Z)
 
-def minima(A,B,C,D):
-    
-    #first find critical point
-    #this is garaunteed to be a unimodal function
+def lerp(A,B,t):
+    global ops
+    ops +=1
+    return add(mul(1-t,A),mul(t,B))
+
+def fusqrt(x):
+    global ops
+    ops += 3 # using fused multiply add
+    # returns upper bound for sqrt
+    return 0.220970869120796 + x*(1.32582521472478 + x*(-0.883883476483184 + x*0.353553390593274))
+
+
+
+# From microsoft research paper for comparison purposes
+# Find vector v given pixel 𝑝=(0,0) and Bézier points b0,b1,b2
+def get_distance_vector(p,b0,b1,b2):
+    global ops
+    a,b,d = det(b0,b2), 2*det(b1,b0), 2*det(b2,b1)          # float a=det(b0,b2), b=2*det(b1,b0), d=2*det(b2,b1);
+    f=b*d-a*a                                               # float f=b*d-a*a;
+    d21,d10,d20 = sub(b2,b1), sub(b1,b0), sub(b2,b0)        # float d21=b2-b1, d10=b1-b0, d20=b2-b0;
+    gf=mul(2,add(add(mul(b,d21),mul(d,d10)),mul(a,d20)))    # float2 gf=2*(b*d21+d*d10+a*d20);
+    gf=(gf[1],-gf[0])                                       # gf=float2(gf.y,-gf.x);
+    pp=mul(-f/dot(gf,gf),gf)                                # float2 pp=-f*gf/dot(gf,gf);
+    d0p=sub(b0,pp)                                          # float2 d0p=b0-pp;
+    ap,bp=det(d0p,d20), 2*det(d10,d0p)                      # float ap=det(d0p,d20), bp=2*det(d10,d0p);
+    # (note that 2*ap+bp+dp=2*a+b+d=4*area(b0,b1,b2))       # 
+    t=clamp((ap+bp)/(2*a+b+d), 0,1)                         # float t=clamp((ap+bp)/(2*a+b+d), 0,1);
+    v=lerp(lerp(b0,b1,t),lerp(b1,b2,t),t)                   # return lerp(lerp(b0,b1,t),lerp(b1,b2,t),t);
+    #return math.sqrt(dot(v,v))
+    ops += 14
+    return math.sqrt(dot(v,v))
+
+
+
+def halley(t0,g,gp,gpp,tol=0.01):
+    global ops
+    t = t0
+    gt = tol + 1
+    iterations = 0
+    # criteria is modified to stop when g is close to zero.
+    # Since we only need the distance to the curve at the closest point, 
+    # it does not matter where precisely the closest point is on the curve
+    # This makes a difference in the number of iterations for areas of the plane that are roughly equidistant to a region of the parabola.  
+    # Specifically, the region of greatest curvature on the parabola
+    while abs(gt) > tol:
+        gt = g(t)
+        gpt = gp(t)
+        gppt = gpp(t)
+        t = t - 2*gt*gpt/(2*gpt*gpt-gt*gppt)
+        ops += 15
+        iterations += 1
+    print("Halley Iterations: {}".format(iterations))
+    return t
+
+def min_distance(A,B,C,D,tol = 0.01):
+    global ops
+    # 2nd order polynomial coefficients for the bezier curve Z
     Alpha = add(sub(A,mul(2,B)),C)
     Beta  = mul(2,sub(B,A))
-    f = lambda t: dist(t,Alpha,Beta,sub(A,B))
-    tol = 0.01
-    tc = gss(f,0,1,tol)
-    P = bez(tc,A,B,C)
-    print(P)
-    #now find which side of the critial line D is on
-    #then minimize distance to D on the sub-interval
-    #I found examples where the "critical point" does not correspond to the middle root
-    f = lambda t: dist(t,Alpha,Beta,sub(A,D))
-    if det(A,B,P)*det(D,B,P) >= 0:
-        print("left")
-        tm = gss(f,0,tc,tol)
-    else:
-        print("right")
-        tm = gss(f,tc,1,tol)
+    Gamma = sub(A,D)
+
+    # 3rd order polynomial coefficients for derivative of distance to the bezier curve "g"
+    h = 2*dot(Alpha,Alpha)
+    i = 3*dot(Alpha,Beta)
+    j = 2*dot(Alpha,Gamma)+dot(Beta,Beta)
+    k = dot(Beta,Gamma)
+    h3 = 3*h
+    h6 = 6*h
+    i2 = 2*i
+    #these can be implemented with the fma instruction to make them more efficient
+    Z   = lambda t: add(Gamma,mul(t,add(Beta,mul(t,Alpha))))
+    g   = lambda t: k+t*(j+t*(i+t*h))
+    gp  = lambda t: j+t*(i2+t*h3)
+    gpp = lambda t: i2+t*h6
     
-    return bez(tm,A,B,C)
+    # v is the vertex of the parabola Z
+    v = -i/h3
+
+    # symmetry about the vertex for zeros of the 2nd derivative of distance
+    phi = v*v-j/h3
     
-"""
-Xiao-Diao Chen, Yin Zhou, Zhenyu Shu, Hua Su, Jean-Claude Paul. Improved Algebraic Algorithm
-On Point Projection For Bézier Curves. Proceedings of the Second International Multi-Symposiums
-on Computer and Computational Sciences (IMSCCS 2007), The University of Iowa, Iowa City, Iowa,
-USA, Aug 2007, Iowa, United States. pp.158-163, ff10.1109/IMSCCS.2007.17ff. ffinria-00518379f
+    g0 = k
+    g1 = h+i+j+k
+    gv = g(v)
+    
+    ops += 15
 
-dot(P-Z(tc),Z'(tc)) = 0
-"""
-
-
-def raphson(A,B,C,D):
-    Alpha = add(sub(A,mul(2,B)),C)
-    Beta  = mul(2,sub(B,A))
-    Gamma = A
-
-    t = 0.0
-    tn = 0.5
-    tol = 0.001
-    while abs(t-tn) > tol:
-        t = tn
-        print(t)
-        Zpp = mul(2,Alpha)
-        Zp = add(Beta,mul(2*t,Alpha))
-        Z = add(Gamma,mul(t,add(Beta,mul(t,Alpha))))
-        V = sub(B,Z)
-        f = dot(V,Zp)
-        print(f)
-        fp = dot(V,Zpp)-dot(Zp,Zp)
-        tn = t - f/fp
+    # when phi > 0, there are local maxima and minima of g.  These could produce zeros.
+    if phi > 0:# could also use g'(v) < 0, but why waste the clock cycles when I need phi anyway
+        sqrtphi = math.sqrt(phi)
         
-    
-    return bez(tn,A,B,C)
+        l = v - sqrtphi
+        r = v + sqrtphi
+        gl = g(l)
+        gr = g(r)
+        
+        lm = ((l > 0) and (gl > 0) and (g0 < 0) and (l < 1)) or ((l > 1) and (g0 < 0) and (g1 > 0))
+        rm = ((r < 1) and (gr < 0) and (g1 > 0) and (r > 0)) or ((r < 0) and (g0 < 0) and (g1 > 0))
+        ls = gv > 0
+        rs = not ls
+        
+        ops += 18
 
-
-def halley(A,B,C,D):
-    Alpha = add(sub(A,mul(2,B)),C)
-    Beta  = mul(2,sub(B,A))
-    Gamma = A
-    minima = []
-
-    for t0 in [0.0,1.0]:
-        tn = t0
-        t = 1-tn
-        tol = 0.001
-        left = tn < 0.5
-        right = tn > 0.5
-        while abs(t-tn) > tol:
-            t = tn        
-            Z   = add(Gamma,mul(t,add(Beta,mul(t,Alpha))))
-            Zp  = add(Beta,mul(2*t,Alpha))
-            Zpp = mul(2,Alpha) #This could be moved out of the loop.  Does not depend on t.
-            DZ  = sub(D,Z)
-            g   = dot(DZ,Zp)# you can check the sign of g on the first pass to see if the root is in the interval
-            gp  = dot(DZ,Zpp)-dot(Zp,Zp)
-            gpp = -3*dot(Zp,Zpp)
-            tn = t - 2*g*gp/(2*gp*gp-g*gpp)
-            print(tn)
-        minima.append((t,dot(DZ,DZ)))
-    
-    #PMLFIXME needs to check and make sure to and t1 are in the interval
-    (t0,d0) = minima[0]
-    (t1,d1) = minima[1]
-
-    if d0 < d1:
-        t = t0
+        if ls and lm:
+            t =  halley(v-2*sqrtphi,g,gp,gpp,tol)
+        elif rs and rm:
+            t =  halley(v+2*sqrtphi,g,gp,gpp,tol)
+        elif ls and rm:
+            m = halley(v+2*sqrtphi,g,gp,gpp,tol)
+            Zm = Z(m)
+            Z0 = sub(A,D)
+            t =  0 if dot(Z0,Z0) < dot(Zm,Zm) else m
+        elif rs and lm:
+            m = halley(v-2*sqrtphi,g,gp,gpp,tol)
+            Zm = Z(m)
+            Z1 = sub(C,D)
+            t =  1 if dot(Z1,Z1) < dot(Zm,Zm) else m
+        else:
+            Z0 = sub(A,D)
+            Z1 = sub(C,D)
+            t =  0 if dot(Z0,Z0) < dot(Z1,Z1) else 1
     else:
-        t = t1
+        if g0 > 0:
+            t =  0
+        elif g1 < 0:
+            t =  1
+        elif gv > 0:
+            t =  halley(0,g,gp,gpp,tol)
+        else:
+            t =  halley(1,g,gp,gpp,tol)
 
-    return bez(t,A,B,C)
+    P = Z(t)
+    return math.sqrt(dot(P,P))
 
-"""
-            # this means the root is not in the interval, and the endpoint is a local minima
-            if left:
-                print("left")
-                left = False
-                if g <= 0.0:
-                    break
-            if right:
-                print("right")
-                right = False
-                if g >= 0.0:
-                    break
-            
-"""
-A = (1.2,1.0)
-B = (1.2,1.4)
-C = (1.6,0.0)
-D = (1.24,1.03)
+
+A = (1.94,0.55)
+B = (2.32,0.6)
+C = (2.42,0.35)
+D = (2.1,0.3)
 #P = minima(A,B,C,D)
 #P = raphson(A,B,C,D)
-P = halley(A,B,C,D)
 
-print(P)
+print("Halley Method")
+ops = 0
+P = min_distance(A,B,C,D)
+print("Min Distance: {}".format(P))
+print("Add/Multiplies: {}".format(ops))
 
+print("Inversion Method")
+ops = 0
+P = get_distance_vector(A,B,C,D)
+print("Min Distance: {}".format(P))
+print("Add/Multiplies: {}".format(ops))
